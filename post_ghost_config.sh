e04da45c13b8ffcd6944e7a833891a2e55365845
David Young <davidy@funkypenguin.co.nz>
2020-11-26 17:55:54 +1300

    Try sneaky plan to get s3 adapter built on every restart (not ideal)

--- post_ghost_config.sh
+++ post_ghost_config.sh
@@ -4,3 +4,8 @@ cp /bitnami/ghost/config.production.json /tmp/config.tmp.json
 jq -r --arg keyId $AWS_ACCESS_KEY_ID --arg accessKey $AWS_ACCESS_SECRET_KEY --arg region $AWS_REGION --arg bucket $AWS_BUCKET \
     '. + { storage: { active: "s3", s3: { accessKeyId: $keyId, secretAccessKey: $accessKey, region: $region, bucket: $bucket } } }' \
     /tmp/config.tmp.json > /bitnami/ghost/config.production.json
+
+cd /bitnami/ghost \
+    && npm i --silent ghost-storage-adapter-s3 \ 
+    && mkdir -p /bitnami/ghost/content/adapters/storage/s3 \
+    && cp -r ./node_modules/ghost-storage-adapter-s3/* /bitnami/ghost/content/adapters/storage/s3/ 
