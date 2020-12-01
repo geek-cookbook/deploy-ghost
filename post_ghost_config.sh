ab145750757d2734690ba38824025a7f97e30378
David Young <davidy@funkypenguin.co.nz>
2020-12-01 14:31:53 +1300

    Debug

--- post_ghost_config.sh
+++ post_ghost_config.sh
@@ -2,7 +2,7 @@
 cp /bitnami/ghost/config.production.json /tmp/config.tmp.json
 
 jq -r --arg keyId $AWS_ACCESS_KEY_ID --arg accessKey $AWS_ACCESS_SECRET_KEY --arg region $AWS_REGION --arg bucket $AWS_BUCKET \
-    '. + { storage: { active: "s3", s3: { accessKeyId: $keyId, secretAccessKey: $accessKey, region: $region, bucket: $bucket, signatureVersion: v4, assetHost: "https://static.funkypenguin.co.nz" } } }' \
+    '. + { storage: { active: "s3", s3: { accessKeyId: $keyId, secretAccessKey: $accessKey, region: $region, bucket: $bucket, signatureVersion: v4, assetHost: \"https://static.funkypenguin.co.nz\" } } }' \
     /tmp/config.tmp.json > /bitnami/ghost/config.production.json
 
 cd /funkypenguin
